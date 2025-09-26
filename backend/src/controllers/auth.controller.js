bcdc193b7c8f0845f296bdb527bff4c66d996352
Andy Li <andy.dang.li@gmail.com>
2025-09-25 23:35:14 -0400

    Implement Welcome Email and Environment Variable Management

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,27 +1,30 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs'; // library to hash passwords
+
+import { ENV } from "../lib/env.js"; // import environment variables
 import { generateToken } from "../lib/utils.js"; // function to generate JWT token
+import { sendWelcomeEmail } from "../emails/emailHandlers.js"; // function to send welcome email
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
 
     try {
         if (!fullName || !email || !password) {
-            return res.status(400).json({message: "All Fields Are Required"});
+            return res.status(400).json({ message: "All Fields Are Required" });
         }
 
         if (password.length < 6) {
-            return res.status(400).json({message: "Password Must Be At Least 6 Characters Long"});
+            return res.status(400).json({ message: "Password Must Be At Least 6 Characters Long" });
         }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // regex for email validation
         if (!emailRegex.test(email)) {
-            return res.status(400).json({message: "Invalid Email Format"});
+            return res.status(400).json({ message: "Invalid Email Format" });
         }
 
         // check if user already exists
-        const user = await User.findOne({email});
-        if (user) return res.status(400).json({message: "Email Already Exists"});
+        const user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: "Email Already Exists" });
 
         // password hashing
         const salt = await bcrypt.genSalt(10);
@@ -31,7 +34,7 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             email,
-            password: hashedPassword
+            password: hashedPassword,
         });
 
         if (newUser) {
@@ -44,13 +47,20 @@ export const signup = async (req, res) => {
                 email: newUser.email,
                 profilePic: newUser.profilePic,
             });
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            }
+            catch (error) {
+                console.error("Error Sending Welcome Email: ", error);
+            }
         }
         else {
-            res.status(400).json({message: "Invalid User Data"});
+            res.status(400).json({ message: "Invalid User Data" });
         }
     }
     catch (error) {
         console.error("Error in Signup Controller: ", error);
-        res.status(500).json({message: "Internal Server Error"});
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'; // library for MongoDB and Node.js, schema-based solution to model and structure data, mapped to MongoDB documents.
+import { ENV } from '../lib/env.js'; // import environment variables
 
 export const connectDB = async () => {
     try {
-        const { MONGO_URI } = process.env;
+        const { MONGO_URI } = ENV;
         if (!MONGO_URI) throw new Error("MONGO_URI is not Defined");
         
         const connection = await mongoose.connect(MONGO_URI)
